75e5aebdaaaa7a1e81dad565d9d372ade389333c
rovxxmjxx <miju3545@gmail.com>
2023-01-07 22:18:33 +0900

    refactor: layout

--- layouts/LoginLayout.tsx
+++ layouts/LoginLayout.tsx
@@ -13,9 +13,9 @@ export default function AuthLayout({ children }: AuthLayout) {
       <PageSEO ogTitle={`Login - ${siteMetaData.title}`} ogDescription={siteMetaData.description} />
       <CommonLayout>
         <div className="flex items-center justify-center h-screen">
-          <div className="grid grid-cols-2 h-3/4 w-full">
+          <div className="grid sm:grid-cols-2 h-3/4 w-full sm:gap-8 xl:gap-16">
             <div className="hidden sm:block bg-blue-200">사진</div>
-            <div className="px-4">
+            <div>
               <h1 className="py-8 text-3xl font-bold">Login</h1>
               <div>{children}</div>
             </div>
